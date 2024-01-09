8910050ea014dbedc16e1cdb6aef80bafd4def91
lance butler <lancej2004@gmail.com>
2024-01-09 12:03:56 -0700

    creates board_rider method and test

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,4 @@
+
 class Ride 
     attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
 
@@ -7,5 +8,26 @@ class Ride
         @admission_fee = attributes[:admission_fee]
         @excitement = attributes[:excitement]
         @total_revenue = 0
+        @riders = Hash.new(0)
+    end
+
+    def board_rider(visitor)
+        return unless visitor.tall_enough?(@min_height)
+
+        visitor_cash= visitor.spending_money - @admission_fee
+
+        return if visitor_cash < 0     
+
+        @riders[visitor] += 1
+        @total_revenue += @admission_fee
+        visitor.spending_money = visitor_cash
+    end
+
+    def rider_log
+        @riders
+    end
+
+    def total_revenue
+        @total_revenue
     end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -21,67 +21,91 @@ RSpec.describe Ride do
         it "exists and has attributes" do
             expect(@ride1.name).to eq("Carousel")
             expect(@ride1.min_height).to eq(24)
-            expect(@ride1.admission_fee).to eq(24)
+            expect(@ride1.admission_fee).to eq(1)
             expect(@ride1.excitement).to eq(:gentle)
             expect(@ride1.total_revenue).to eq (0)
         end
     end
 
+    describe '#board_rider' do
+      it 'adds the visitor to the ride and updates total revenue' do
+        allow(@visitor1).to receive(:tall_enough?).and_return(true)
 
+        expect { @ride1.board_rider(@visitor1) }.to change { @ride1.rider_log[@visitor1] }.from(0).to(1)
+        expect(@ride1.total_revenue).to eq(1)
+      end    
 
+      it 'does not add the visitor to the ride and does not update total revenue' do
+        allow(@visitor2).to receive(:tall_enough?).and_return(false)
 
-@visitor1.add_preference(:gentle)
+        expect { @ride1.board_rider(@visitor2) }.not_to(change { @ride1.rider_log[@visitor2] })
+        expect(@ride1.total_revenue).to eq(0)
+      end
+    end
 
-@visitor2.add_preference(:gentle)
+    describe '#rider_log' do
+        it "makes a log of visitors at rides and updates total revenue" do
+            @visitor1.add_preference(:gentle)
+            @visitor2.add_preference(:gentle)
+            
+            @ride1.board_rider(@visitor1)
+            @ride1.board_rider(@visitor2)
+            @ride1.board_rider(@visitor1)
 
-@ride1.board_rider(visitor1)
+            expect(@ride1.rider_log).to be_a(Hash)
+            expect(@visitor1.spending_money).to eq(8)
+            expect(@visitor2.spending_money).to eq(4)
+            expect(@ride1.total_revenue).to eq(3)
+        end
+    end
 
-@ride1.board_rider(visitor2)
+    # describe '#total_revenue' do
+    #     it "returns correct total revenue for rides" do
+    #         @ride1.board_rider(@visitor1)
+    #         @ride1.board_rider(@visitor2)
+    #         @ride1.board_rider(@visitor1)
 
-@ride1.board_rider(visitor1)
-
-@ride1.rider_log
-#=> {#<Visitor:0x000000015a16e918 @height=54, @name="Bruce", @preferences=[:gentle], @spending_money=8> => 2,
- #<Visitor:0x000000015a11c5c8 @height=36, @name="Tucker", @preferences=[:gentle], @spending_money=4> => 1}
-
- @visitor1.spending_money
+            
+    #         expect(@visitor1.spending_money).to eq(8)
+    #         expect(@visitor2.spending_money).to eq(4)
+    #         expect(@ride1.total_revenue).to eq(3)
+    #     end
+    # end
  #=> 8
  
- @visitor2.spending_money
  #=> 4
  
- @ride1.total_revenue
  #=> 3
  
- #=> #<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[], @spending_money=15>
+#  #=> #<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[], @spending_money=15>
  
- #=> #<Ride:0x0000000159a0cd00 @admission_fee=5, @excitement=:gentle, @min_height=36, @name="Ferris Wheel", @rider_log={}>
+#  #=> #<Ride:0x0000000159a0cd00 @admission_fee=5, @excitement=:gentle, @min_height=36, @name="Ferris Wheel", @rider_log={}>
  
- #=> #<Ride:0x0000000159ae7a68 @admission_fee=2, @excitement=:thrilling, @min_height=54, @name="Roller Coaster", @rider_log={}>
+#  #=> #<Ride:0x0000000159ae7a68 @admission_fee=2, @excitement=:thrilling, @min_height=54, @name="Roller Coaster", @rider_log={}>
  
- @visitor2.add_preference(:thrilling)
- #=> [:gentle, :thrilling]
+#  @visitor2.add_preference(:thrilling)
+#  #=> [:gentle, :thrilling]
  
- @visitor3.add_preference(:thrilling)
- #=> [:thrilling]
+#  @visitor3.add_preference(:thrilling)
+#  #=> [:thrilling]
  
- @ride3.board_rider(visitor1)
+#  @ride3.board_rider(visitor1)
  
- @ride3.board_rider(visitor2)
+#  @ride3.board_rider(visitor2)
  
- @ride3.board_rider(visitor3)
+#  @ride3.board_rider(visitor3)
  
- @visitor1.spending_money
- #=> 8
+#  @visitor1.spending_money
+#  #=> 8
  
- @visitor2.spending_money
- #=> 4
- @visitor3.spending_money
-#=> 13
+#  @visitor2.spending_money
+#  #=> 4
+#  @visitor3.spending_money
+# #=> 13
 
-@ride3.rider_log
-#=> {#<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[:thrilling], @spending_money=13> => 1}
+# @ride3.rider_log
+# #=> {#<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[:thrilling], @spending_money=13> => 1}
 
-@ride3.total_revenue
-#=> 2
+# @ride3.total_revenue
+# #=> 2
 end
